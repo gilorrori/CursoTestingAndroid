a6884dd74162bf6dbbf4e3956b98de669b0bb08f
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-12 12:15:05 -0600

    se agrega la inyeccion a la primer feature

---
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/presentation/ProductListScreen.kt
@@ -0,0 +1,10 @@
+package com.gilorroristore.cursotestingandroid.productlist.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+
+@Composable
+fun ProductListScreen(
+    productListViewModel: ProductListViewModel = hiltViewModel()
+) {
+}
---
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/presentation/ProductListViewModel.kt
@@ -0,0 +1,8 @@
+package com.gilorroristore.cursotestingandroid.productlist.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductListViewModel @Inject constructor() : ViewModel() {}

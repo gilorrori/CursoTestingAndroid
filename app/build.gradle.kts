55e4133f2bb05911f2aa6628c4c8930a13fc2eb6
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-12 11:57:30 -0600

    se preparan dependencias para el proyecto de test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.devtools.ksp)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -29,8 +32,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
@@ -38,6 +41,7 @@ android {
 }
 
 dependencies {
+    //Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -46,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    //Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -54,6 +60,44 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
+    //Retrofit
+    implementation (libs.retrofit)
+    implementation (libs.okhttp)
+    implementation (libs.okhttp.logging)
+    implementation (libs.converter.kotlinx.serialization)
 
+    //Icons
+    implementation (libs.androidx.compose.material.icons.extended)
+    //Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 
+    //Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.hilt.lifecycle.viewmodel.compose)
+    ksp(libs.hilt.compiler.ksp)
+
+    //Navigation
+    implementation(libs.navigation3.ui)
+    implementation(libs.navigation3.runtime)
+
+    //Room
+    implementation(libs.room.ktx)
+    implementation (libs.room.runtime)
+    ksp(libs.room.compiler.ksp)
+
+    //DataStore
+    implementation(libs.androidx.datastore.preferences.android)
+    implementation(libs.androidx.datastore.preferences.core)
+
+    //Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    //Viewmodel
+    implementation(libs.lifecycle.viewmodel.compose)
 }
--- app/src/main/java/com/gilorroristore/cursotestingandroid/MainActivity.kt
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CursoTestingAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -27,5 +27,21 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CursoTestingAndroidTheme {
+        Greeting("Android")
     }
 }

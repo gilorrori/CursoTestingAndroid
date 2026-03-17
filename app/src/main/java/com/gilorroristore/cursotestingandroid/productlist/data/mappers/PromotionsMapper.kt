40b8d4d87ddecc85c1a1e7fbf14ec1d96141141b
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-17 14:18:37 -0600

    promotion is stored  in room and validations for PromotionEntity.kt to Promotion.kt

--- app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/data/mappers/PromotionsMapper.kt
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/data/mappers/PromotionsMapper.kt
@@ -2,9 +2,12 @@ package com.gilorroristore.cursotestingandroid.productlist.data.mappers
 
 import com.gilorroristore.cursotestingandroid.productlist.data.local.database.entities.PromotionEntity
 import com.gilorroristore.cursotestingandroid.productlist.data.response.PromotionResponse
+import com.gilorroristore.cursotestingandroid.productlist.domain.models.Promotion
+import com.gilorroristore.cursotestingandroid.productlist.domain.models.PromotionType
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
+import java.time.Instant
 
 fun PromotionResponse.toEntity(json: Json): PromotionEntity? {
 
@@ -28,5 +31,42 @@ fun PromotionResponse.toEntity(json: Json): PromotionEntity? {
         payY = payY,
         startAtEpoch = startAtEpoch,
         endAtEpoch = endAtEpoch
+    )
+}
+
+fun PromotionEntity.toDomain(json: Json): Promotion? {
+
+    val decodeProductIds = runCatching {
+        json.decodeFromString(
+            ListSerializer(String.serializer()), productIds
+        )
+    }.getOrNull()
+
+
+    // Si el backend no envia un type que haga match con el enum que se tiene, le mandamos uno por defecto con runcatching
+    val finalType = runCatching {
+        PromotionType.valueOf(
+            type.trim().uppercase()
+        )
+    }.getOrNull()
+
+    if (finalType == null ||decodeProductIds == null) return null
+
+    val finalOfferValue = when(finalType) {
+        PromotionType.PERCENT -> percent
+        PromotionType.BUY_X_PAY_X -> payY
+    }?.toDouble()
+
+    /* Misma validacion si finalPrice es null retorna un promotion null */
+    finalOfferValue ?: return null
+
+    return Promotion(
+        id = id,
+        type = finalType,
+        productIds = decodeProductIds,
+        value = finalOfferValue,
+
+        startTime = Instant.ofEpochSecond(startAtEpoch),
+        endTime = Instant.ofEpochSecond(endAtEpoch),
     )
 }
--- app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/data/repositories/PromotionRepositoryImpl.kt
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/data/repositories/PromotionRepositoryImpl.kt
@@ -3,11 +3,20 @@ package com.gilorroristore.cursotestingandroid.productlist.data.repositories
 import com.gilorroristore.cursotestingandroid.core.domain.coroutines.DispatchersProviders
 import com.gilorroristore.cursotestingandroid.productlist.data.local.LocalDataSource
 import com.gilorroristore.cursotestingandroid.productlist.data.local.database.entities.PromotionEntity
+import com.gilorroristore.cursotestingandroid.productlist.data.mappers.toDomain
 import com.gilorroristore.cursotestingandroid.productlist.data.mappers.toEntity
 import com.gilorroristore.cursotestingandroid.productlist.data.remote.RemoteDataSource
 import com.gilorroristore.cursotestingandroid.productlist.domain.models.Promotion
 import com.gilorroristore.cursotestingandroid.productlist.domain.repositories.PromotionRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
@@ -18,14 +27,37 @@ class PromotionRepositoryImpl @Inject constructor(
     val dispatchers: DispatchersProviders,
     val json: Json
 ) : PromotionRepository {
+
+    private val refreshScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private val refreshMutex = Mutex()
+
     override fun getActivePromotions(): Flow<List<Promotion>> {
-        TODO()
+        return localDataSource.getAllPromotions()
+            .map { promotionEntities -> promotionEntities.mapNotNull { it.toDomain(json) } }
+
+            .onStart {
+                refreshScope.launch {
+                    if (!refreshMutex.tryLock()) return@launch
+
+                    try {
+                        refreshPromotions()
+                    } catch (e: Exception) {
+
+                    } finally {
+                        refreshMutex.unlock()
+                    }
+                }
+            }
+            .catch {
+                // Error desde la funcion getallProducts
+            }
     }
 
     override suspend fun refreshPromotions() {
         withContext(dispatchers.io) {
             val promotions = remoteDataSource.getPromotions().getOrThrow()
-            val promotionsEntity: List<PromotionEntity> = promotions.mapNotNull { it.toEntity(json) }
+            val promotionsEntity: List<PromotionEntity> =
+                promotions.mapNotNull { it.toEntity(json) }
             localDataSource.savePromotions(promotionsEntity)
         }
     }

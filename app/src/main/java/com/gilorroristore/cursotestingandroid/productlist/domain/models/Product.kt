095a6359754439dbc80820f3a7c5ce3b5af57190
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-16 20:15:07 -0600

    new enum and minor update in parameter name

--- app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/domain/models/Product.kt
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/domain/models/Product.kt
@@ -4,7 +4,7 @@ data class Product(
     val id: String,
     val name: String,
     val description: String,
-    val priceCents: Double,
+    val price: Double,
     val category: String,
     val stock: Int,
     val imageUrl: String? = null
---
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/domain/models/SortOption.kt
@@ -0,0 +1,8 @@
+package com.gilorroristore.cursotestingandroid.productlist.domain.models
+
+enum class SortOption {
+    PRICE_ASC,
+    PRICE_DESC,
+    DISCOUNT,
+    NONE
+}

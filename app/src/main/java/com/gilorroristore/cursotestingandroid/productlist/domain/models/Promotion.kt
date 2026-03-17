0c80b3160eda00af6417705c7521291bce93152c
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-17 13:22:11 -0600

    new promotion final model

--- app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/domain/models/Promotion.kt
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/domain/models/Promotion.kt
@@ -1,12 +1,21 @@
+@file:OptIn(ExperimentalTime::class)
+
 package com.gilorroristore.cursotestingandroid.productlist.domain.models
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
+enum class PromotionType {
+    PERCENT,
+    BUY_X_PAY_X
+}
 
 data class Promotion(
     val id: String,
-    val productId: String,
-    val type: String,
-    val percent: Int? = null,
-    val buyX: Int? = null,
-    val payY: Int? = null,
-    val startAtEpoch: Long? = null,
-    val endAtEpoch: Long? = null
+    val type: PromotionType,
+    val productIds: List<String>,
+    val value: Double,
+    val buyQuantity: Int? = null,
+    val startTime: Instant,
+    val endTime: Instant
 )

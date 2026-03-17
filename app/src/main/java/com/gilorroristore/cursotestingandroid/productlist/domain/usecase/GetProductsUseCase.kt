67c0da188e5aa32514006410493d920e3f7fa5ba
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-17 14:13:53 -0600

    Repository injected in GetProductsUseCase.kt

--- app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/domain/usecase/GetProductsUseCase.kt
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/domain/usecase/GetProductsUseCase.kt
@@ -2,11 +2,13 @@ package com.gilorroristore.cursotestingandroid.productlist.domain.usecase
 
 import com.gilorroristore.cursotestingandroid.productlist.domain.models.Product
 import com.gilorroristore.cursotestingandroid.productlist.domain.repositories.ProductRepository
+import com.gilorroristore.cursotestingandroid.productlist.domain.repositories.PromotionRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetProductsUseCase @Inject constructor(
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val promotionRepository: PromotionRepository
 ) {
     operator fun invoke() : Flow<List<Product>>{
         return productRepository.getProducts()

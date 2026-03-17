af5c5b2170393e6c2a6723151017109d14f425af
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-17 11:24:49 -0600

    class for UiStates product screen

--- app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/presentation/ProductListUiState.kt
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/productlist/presentation/ProductListUiState.kt
@@ -1,14 +1,15 @@
 package com.gilorroristore.cursotestingandroid.productlist.presentation
 
 import com.gilorroristore.cursotestingandroid.productlist.domain.models.Product
+import com.gilorroristore.cursotestingandroid.productlist.domain.models.SortOption
 
 sealed class ProductListUiState {
     data object Loading: ProductListUiState()
     data class Error(val message: String) : ProductListUiState()
     data class Success(
         val products: List<Product>,
-        //val categories: List<String>,
-        //val selectedCategory: String,
-        //sortOption
+        val categories: List<String>?,
+        val selectedCategory: String?,
+        val sortOption: SortOption?
     ) : ProductListUiState()
 }

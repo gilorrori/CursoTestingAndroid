8dfdec2305de958fda86abf8746a85c8544c9629
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-18 17:34:30 -0600

    Generic app top bar created for entire application

---
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/core/presentation/components/MarketTopAppBar.kt
@@ -0,0 +1,40 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.gilorroristore.cursotestingandroid.core.presentation.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MarketTopAppBar(
+    title: String,
+    onBackSelected: () -> Unit
+) {
+
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(
+                onClick = { onBackSelected() }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Nav back",
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ),
+    )
+}

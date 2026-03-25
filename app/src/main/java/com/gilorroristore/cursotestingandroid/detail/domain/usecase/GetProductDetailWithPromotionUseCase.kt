98bf6fe4de57c4bb2c42d40135585d584b0998f0
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-24 22:13:28 -0600

    new useCase, useful to get product with promotion

---
+++ app/src/main/java/com/gilorroristore/cursotestingandroid/detail/domain/usecase/GetProductDetailWithPromotionUseCase.kt
@@ -0,0 +1,33 @@
+package com.gilorroristore.cursotestingandroid.detail.domain.usecase
+
+import com.gilorroristore.cursotestingandroid.productlist.domain.models.ProductWithPromotion
+import com.gilorroristore.cursotestingandroid.productlist.domain.repositories.ProductRepository
+import com.gilorroristore.cursotestingandroid.productlist.domain.repositories.PromotionRepository
+import com.gilorroristore.cursotestingandroid.productlist.domain.usecase.GetPromotionForProduct
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import java.time.Instant
+import javax.inject.Inject
+
+class GetProductDetailWithPromotionUseCase @Inject constructor(
+    private val productRepository: ProductRepository,
+    private val promotionRepository: PromotionRepository,
+    private val getPromotionForProduct: GetPromotionForProduct
+) {
+    operator fun invoke(productId: String): Flow<ProductWithPromotion?> {
+        return combine(
+            productRepository.getProductById(productId),
+            promotionRepository.getActivePromotions()
+        ) { product, promotions ->
+            val now = Instant.now()
+            val activePromotions = promotions.filter {
+                it.startTime <= now && it.endTime >= now
+            }
+
+            product?.let {
+                val finalPromotion = getPromotionForProduct(product, activePromotions)
+                ProductWithPromotion(product = it, promotion = finalPromotion)
+            }
+        }
+    }
+}
